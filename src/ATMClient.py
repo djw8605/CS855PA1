1414733b88f76a3c7f609a79d1505ed6d94db4ed
Derek Weitzel <djw8605@gmail.com>
2013-04-19 00:08:33 -0500

    Adding changes for PA3

--- src/ATMClient.py
+++ src/ATMClient.py
@@ -28,9 +28,7 @@ amount
 
 import xmlrpclib
 import sys
-
-
-
+import time
 
 
 def usage():
@@ -58,42 +56,118 @@ def main():
     server = xmlrpclib.ServerProxy("http://%s:%i" % (remote_server, remote_port), )
     
     try:
-        account_number = int(sys.argv[4])
+        customer_number = int(sys.argv[4])
     except:
         print "Unable to convert account number to number: %s" % sys.argv[4]
         sys.exit(1)
     
+
+    
     # Inquiry operation
     if operation == "inquiry":
-        return_dict = server.inquiry(account_number)
+        
+        try:
+            account_number = int(sys.argv[5])
+        except: 
+            print "Unable to convert account number to number: %s" % sys.argv[5]
+            sys.exit(1)
+        
+        while True:
+            return_dict = server.inquiry(customer_number, account_number)
+            if return_dict['status'] == 'WAIT':
+                time.sleep(1)
+                continue
+            else:
+                break
+                
         if return_dict['status'] == 'OK':
             print "Current value of account %i is $%.2f" % (account_number, float(return_dict['result']))
         else:
             print "Error from ATM Server: %s" % return_dict['reason']
         
     elif operation == "deposit":
+        
         try:
-            account_modify = float(sys.argv[5])
-        except:
-            print "Unable to convert money to number: %s" % sys.argv[5]
+            account_number = int(sys.argv[5])
+        except: 
+            print "Unable to convert account number to number: %s" % sys.argv[5]
             sys.exit(1)
-        return_dict = server.deposit(account_number, account_modify)
+        
+        try:
+            account_modify = float(sys.argv[6])
+        except:
+            print "Unable to convert money to number: %s" % sys.argv[6]
+            sys.exit(1)
+            
+        while True:
+            return_dict = server.deposit(customer_number, account_number, account_modify)
+            if return_dict['status'] == 'WAIT':
+                time.sleep(1)
+                continue
+            else:
+                break
+                
         if return_dict['status'] == 'OK':
             print "Successful deposit of $%.2f to account %i" % (account_modify, account_number)
         else:
             print "Error from ATM Server: %s" % return_dict['reason']
         
     elif operation == "withdraw":
+        
         try:
-            account_modify = float(sys.argv[5])
-        except:
-            print "Unable to convert money to number: %s" % sys.argv[5]
+            account_number = int(sys.argv[5])
+        except: 
+            print "Unable to convert account number to number: %s" % sys.argv[5]
             sys.exit(1)
-        return_dict = server.withdraw(account_number, account_modify)
+        
+        try:
+            account_modify = float(sys.argv[6])
+        except:
+            print "Unable to convert money to number: %s" % sys.argv[6]
+            sys.exit(1)
+        
+        while True:
+            
+            return_dict = server.withdraw(customer_number, account_number, account_modify)
+            if return_dict['status'] == 'WAIT':
+                time.sleep(1)
+                continue
+            else:
+                break
+                
         if return_dict['status'] == 'OK':
             print "Successful withdraw of $%.2f to account %i" % (account_modify, account_number)
         else:
             print "Error from ATM Server: %s" % return_dict['reason']
+    
+    elif operation == "begin_transaction":
+        # do nothing
+        print "Customer %i starts their transaction" % customer_number
+        
+    elif operation == "end_transaction":
+        
+        while True:
+            return_dict = server.commit_transaction(customer_number)
+            if return_dict['status'] == 'WAIT':
+                time.sleep(1)
+                continue
+            else:
+                break
+        
+        print "Customer %i commits their transaction" % customer_number
+        
+    
+    elif operation == "abort_transaction":
+        
+        while True:
+            return_dict = server.abort_transaction(customer_number)
+            if return_dict['status'] == 'WAIT':
+                time.sleep(1)
+                continue
+            else:
+                break
+        print "Customer %i aborts their transaction" % customer_number
+        
     
     else:
         print "Unknown operation: %s" % operation
--- src/ATMServer.py
+++ src/ATMServer.py
@@ -1,5 +1,91 @@
 #!/usr/bin/python
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import threading
+    
+
+
+
+class Account:
+    """
+    A lockable account in the ATM
+    
+    """
+    def __init__(self, id, amount):
+        self.id = id
+        self.amount = float(amount)
+        self.tmp_amount = self.amount
+        self.locked = False
+        self.locked_customer = -1
+        self._lock = threading.Lock()
+
+        
+        self.lock_queue = []
+        
+    def lock(self, customer):
+        
+        self._lock.acquire()
+        if customer == self.locked_customer:
+            self._lock.release()
+            return True
+        
+        if self.locked == False:
+            # Check the queue
+            if len(self.lock_queue):
+                print self.lock_queue
+                if customer == self.lock_queue[0]:
+                    # Your the next in the queue, so return
+                    self.lock_queue.pop(0)
+                    self.locked_customer = customer
+                    self.locked = True
+                    self._lock.release()
+                    return True
+                else:
+                    # Your not the next in the queue
+                    if customer in self.lock_queue:
+                        self._lock.release()
+                        return False
+                    else:
+                        self.lock_queue.append(customer)
+                        self._lock.release()
+                        return False
+            else:
+                # If queue is empty, then you got the lock
+                self.locked = True
+                self.locked_customer = customer
+                self._lock.release()
+                return True
+        else:
+            # If the lock is already taken, put yourself on the queue
+            # Put the thread in a queue
+            if customer not in self.lock_queue:
+                self.lock_queue.append(customer)
+            self._lock.release()
+            return False
+            
+    def unlock(self, customer):
+        self._lock.acquire()
+        if self.locked == True:
+            self.locked = False
+        self._lock.release()
+    
+    def deposit(self, amount):
+        self.tmp_amount += float(amount)
+        
+    def withdraw(self, amount):
+        self.tmp_amount -= float(amount)
+        
+    def inquiry(self):
+        return float(self.tmp_amount)
+        
+    def commit(self):
+        self.amount = self.tmp_amount 
+        
+    def abort(self):
+        self.tmp_amount = self.amount
+        
+        
+        
+    
 
 class ATMServer:
     """
@@ -10,12 +96,21 @@ class ATMServer:
     def __init__(self):
         
         # Initialized the starting account
-        self.accounts = {100: 1000.0}
+        self.accounts = {100: Account(100, 1000.0),
+                         200: Account(100, 1000.0)}
+                         
+        self.customer = {}
         
         
-    def deposit(self, account, amount):
+    def _initialize_customer(self, customer):
+        if customer not in self.customer:
+            self.customer[int(customer)] = []
+        
+        
+    def deposit(self, customer, account, amount):
         """Deposit money into the account.
         
+        :param int customer: Customer number
         :param int account: Account number to deposit money into
         :param float amount: value to be added to the account
             
@@ -24,16 +119,25 @@ class ATMServer:
         This function will fail if the account does not exist.
         
         """
+        
+        self._initialize_customer(customer)
         if account not in self.accounts:
             return { 'status': 'ERROR', 'reason': "Account %s was not found" % account }
+            
+        if not self.accounts[int(account)].lock(customer):
+            return { 'status': 'WAIT' }
+        else:
+            if int(account) not in self.customer[int(customer)]:
+                self.customer[int(customer)].append(account)
         
-        self.accounts[int(account)] += float(amount)
+        self.accounts[int(account)].deposit(float(amount))
         return { 'status': 'OK' }
 
         
-    def withdraw(self, account, amount):
+    def withdraw(self, customer, account, amount):
         """Withdraw money from the account
         
+        :param int customer: Customer number
         :param int account: Account number to deposit money into
         :param float amount: value to be added to the account
             
@@ -42,31 +146,82 @@ class ATMServer:
         This function will fail if the account does not have sufficient funds to make the withdraw.
         
         """
+        self._initialize_customer(customer)
         if account not in self.accounts:
             return { 'status': 'ERROR', 'reason': "Account %s was not found" % account }
+            
+        if not self.accounts[int(account)].lock(customer):
+            return { 'status': 'WAIT' }
+        else:
+            if int(account) not in self.customer[int(customer)]:
+                self.customer[int(customer)].append(account)
         
-        if  self.accounts[int(account)] >= float(amount):
-            self.accounts[int(account)] -= float(amount)
+        if  self.accounts[int(account)].inquiry() >= float(amount):
+            self.accounts[int(account)].withdraw(float(amount))
             return { 'status': 'OK' }
         else:
             return { 'status': 'ERROR', 'reason': "Insufficient Funds"}
 
         
-    def inquiry(self, account):
+    def inquiry(self, customer, account):
         """
         Inquire the account amount
         
+        :param int customer: Customer number
         :param int account: Account number to deposit money into
             
         :rtype: status dict, see: :ref:`Status Dictionary <status-dict>`
         
         """
+        self._initialize_customer(customer)
         if account in self.accounts:
-            return { 'status': 'OK', 'result': self.accounts[account] }
+            
+            if not self.accounts[int(account)].lock(customer):
+                return { 'status': 'WAIT' }
+            else:
+                if int(account) not in self.customer[int(customer)]:
+                    self.customer[int(customer)].append(account)
+                
+                return { 'status': 'OK', 'result': self.accounts[account].inquiry() }
         else:
             return { 'status': 'ERROR', 'reason': "Unknown account %s" % account }
+            
 
+    def commit_transaction(self, customer):
+        """
+        Commit a transaction for the customer
         
+        :param int customer: Customer number
+            
+        :rtype: status dict, see: :ref:`Status Dictionary <status-dict>`
+        
+        """
+        self._initialize_customer(customer)
+        for account in self.customer[int(customer)]:
+            self.accounts[int(account)].commit()
+            self.accounts[int(account)].unlock(customer)
+            self.customer[int(customer)].remove(account)
+        
+        return { 'status': 'OK' }
+            
+            
+    def abort_transaction(self, customer):
+        """
+        Abort a transaction for the customer
+        
+        :param int customer: Customer number
+            
+        :rtype: status dict, see: :ref:`Status Dictionary <status-dict>`
+        
+        """
+        self._initialize_customer(customer)
+        for account in self.customer[int(customer)]:
+            self.accounts[int(account)].abort()
+            self.accounts[int(account)].unlock(customer)
+            self.customer[int(customer)].remove(account)
+    
+        return { 'status': 'OK' }
+    
         
 
 def main():
